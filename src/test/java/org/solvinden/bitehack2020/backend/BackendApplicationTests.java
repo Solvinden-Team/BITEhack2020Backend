ed6a8918980a5f97637c43967cc6d0d377fbc721
Paweł Taborowski <peantab@gmail.com>
2020-01-11 14:14:48 +0100

    Basic project setup

---
+++ src/test/java/org/solvinden/bitehack2020/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solvinden.bitehack2020.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
